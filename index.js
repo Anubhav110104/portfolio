4c6917c8e62916417f84bed5db9c84f065103c8c
Anubhav110104 <149408880+Anubhav110104@users.noreply.github.com>
2023-11-01 13:33:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var typed = new Typed('#element', {
+    strings: ['Front-End Developer', 'Web Designer', 'Programmer', 'UI/UX Designer'],
+    typeSpeed: 50,
+     backSpeed:60,
+     backDelay:500,
+     loop:true
+     
+});
+
+//   active menu
+
+// let menuLi = document.querySelectorAll('nav ul li a');
+// let section = document.querySelectorAll('section');
+
+// function activeMenu (){
+//     let len = section.length;
+//     while(--len && window.scrollY + 97 < section[len].offsetTop){}
+//     menuLi.forEach(sec => sec.classList.remove("active"));
+//     menuLi[len].classList.add("active");
+// }
+
+// activeMenu();
+// window.addEventListener("scroll",activeMenu);
